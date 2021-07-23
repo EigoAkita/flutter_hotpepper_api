6b370e8e36481e22155a3667a0195939a9e6b6cd
英吾 <mono.eigo@gmail.com>
2021-07-24 08:37:52 +0900

    コメントアウト

--- lib/main.dart
+++ lib/main.dart
@@ -48,18 +48,18 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headline4,
+            // ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        tooltip: 'インクリメント',
+        child: Icon(Icons.airplanemode_on_sharp),
+      ),
     );
   }
 }
