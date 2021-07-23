741252145fc95834b33084ba20bfe78e4bd430be
英吾 <mono.eigo@gmail.com>
2021-07-24 08:19:20 +0900

    Revert "文字の変更"
    This reverts commit 752cc7b58b84c9d4a8950fee8339fba5ffc1812a.

--- lib/main.dart
+++ lib/main.dart
@@ -57,9 +57,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'インクリメント　　　　',
+        tooltip: 'Increment',
         child: Icon(Icons.add),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
